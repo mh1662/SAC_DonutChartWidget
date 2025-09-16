1d3aaaa0c6966aed415f6daba467c275862de180
Park MH <26458540+mh1662@users.noreply.github.com>
2025-09-16 15:33:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,77 +1,23 @@
-var getScriptPromisify = (src) => {
-	return new Promise((resolve) => {
-		$.getScript(src, resolve)
-	})
-}
+(function () {
+	const template = document.createElement('template')
+	template.innerHTML = `
+		<style>
+		</style>
+		<div id="root" style="width: 100%; height: 100%;">
+		커스텀위젯테스트
+		</div>
+	`
 
-var parseMetadata = metadata => {
-	const {dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata
-	const dimensions = []
-	for (const key in dimensionsMap) {
-		const dimension = dimensionsMap[key]
-		dimensions.push({key, ...dimension})
-	}
-	const measures = []
-	for (const key in measuresMap){
-		const measure = measuresMap[key]
-		measures.push({key, ...measure})
-	}
-	return {dimensions, measures, dimensionsMap, measuresMap}
-}
+	class Main extends HTMLElement{
+		constructor () {
+			super()
 
-class Main extends HTMLElement {
-	constructor () {
-		super()
+			this._shadowRoot = this.attachShadow({mode: 'open'})
+			this._shadowRoot.appendChild(template.content.cloneNode(true))
 
-		this._shadowRoot = this.attachShadow({mode:'open'})
-		this._shadowRoot.appendChild(template.content.cloneNode(true))
-
-		this._root = this._shadowRoot.getElementById('root')
-
-		this._eChart = null
-	}
-
-	async render () {
-		const dataBinding = this.dataBinding
-		if (!dataBinding || dataBinding.state !== 'success') {return}
-
-		await getScriptPromisify('https://cdn.staticfile.org/echarts/5.0.0/echarts.min.js')
-
-		const {data, metadata} = dataBinding
-		const {dimensions,measures} = parseMetadata(metadata)
-
-		//dimension
-		const categoryData = []
-
-		//measures
-		const series = measures.map(measure => {
-			return {
-				id: measure.id,
-				name: measure.label,
-				data: [],
-				key: measure.key,
-				type: 'line',
-				smooth: true
-			}
-		})
-
-		data.forEach(row => {
-			categoryData.push(dimensions.map(dimension => {
-				return row[dimension.key].label
-			}).join('/'))		//dimension
-			series.forEach(series => {
-				series.data.push(row[series.key].raw)
-			})					// measures
-		})
-
-		if (this._eChart) {echarts.dispose(this._eChart)}
-		const eChart = this._eChart = echarts.init(this._root, 'main')
-		const option = {
-			xAxis: {type: 'category', data: categoryData},
-			yAxis: {type: 'value'},
-			tooltip: {trigger: 'axis'},
-			series
+			this._root = this._shadowRoot.getElementById('root')
 		}
-		eChart.setOption(option)
 	}
-}
+
+	customElements.define('donut-chart', Main)
+})()
